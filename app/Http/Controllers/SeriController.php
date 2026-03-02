b4dd804ebb4f9f0c90f367cc9987b6dbc162d0eb
Dell <dafayudistira24@gmail.com>
2026-03-02 14:31:33 +0700

    nomor seri up

--- app/Http/Controllers/SeriController.php
+++ app/Http/Controllers/SeriController.php
@@ -30,7 +30,7 @@ class SeriController extends Controller
    public function store(Request $request)
 {
     $validated = $request->validate([
-        'nomor_seri' => 'required|unique:seri,nomor_seri',
+        'nomor_seri' => 'required',
         'sku'        => 'required',
     ]);
 
